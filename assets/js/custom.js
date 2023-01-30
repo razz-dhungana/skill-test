ca1152076c31f9ad648d35c401f2191ab074e6c7
githubrazz <razzdhungana49@outlook.com>
2023-01-30 10:48:00 -0800

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+	    jQuery('header nav').meanmenu({
+	    	meanMenuOpen: "<span></span><span></span><span></span>",
+	    	meanMenuContainer: 'header',
+	    	meanScreenWidth: 991
+
+	    });
+	});
